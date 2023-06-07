4bc052ebec5a13d861df091ee95e84e4304b4ea0
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-06-07 19:04:57 +0000

    Thu, Jun 8, 2023, 3:04 AM +08:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,7 +1,12 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int n;
+    do
+    {
+        n = get_int("how high is the tower?\n");
+    }
+    while (n <1);
 }
