e6b20322f8d2fd10ef7a70db01f405916b712572
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-08-21 16:13:22 +0000

    Tue, Aug 22, 2023, 12:13 AM +08:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -19,6 +19,13 @@ int main(void)
     int score2 = compute_score(word2);
 
     // TODO: Print the winner
+    if
+    {
+        word1 > word2
+    }
+    printf(Player 1 wins!)
+    else
+    
 }
 
 int compute_score(string word)
