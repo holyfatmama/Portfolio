9664a934d8d4d9de67f2e5cb5b6892bced54c0bc
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-08-24 16:32:19 +0000

    Fri, Aug 25, 2023, 12:32 AM +08:00

--- readability/readability.c
+++ readability/readability.c
@@ -10,6 +10,9 @@ int main (void)
     string text = get_string("Text: ");
     printf("%s\n", text);
 
-    int letters = count_letters(text);
+}
 
+int count_letters(string text)
+{
+    int letters = count_letters(string text)
 }
