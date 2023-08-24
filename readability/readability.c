0321ac14ff2408bf910b28952717cacfee9e84b9
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-08-24 18:49:36 +0000

    Fri, Aug 25, 2023, 2:49 AM +08:00

--- readability/readability.c
+++ readability/readability.c
@@ -22,7 +22,7 @@ int main (void)
     float L = letters / words * 100;
     float S = sentences / words * 100;
 
-    int index = 0.0588 * L - 0.296 * S - 15.8;
+    int index = round(0.0588 * L - 0.296 * S - 15.8);
 
     if (index < 1)
     {
