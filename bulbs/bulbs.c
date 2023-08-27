1e37b508c9558ebb47641b73a85cb7d56730bcfb
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-08-27 17:22:48 +0000

    Mon, Aug 28, 2023, 1:22 AM +08:00

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -9,37 +9,33 @@ void print_bulb(int bit);
 int main(void)
 {
     // Prompt user for input
-
     string message = get_string("Message:\n");
 
     // Go thru each of the characters of string message
-
     for (int i = 0, n = strlen(message); i < n; i++)
     {
         int binary[] = {0, 0, 0, 0, 0, 0, 0, 0};
 
         // convert decimal to ASCII
-
         int decimal = message [i];
 
         // Convert ASCII into binary
         int j = 0;
 
         while (decimal > 0)
-         {
+        {
             binary[j] = decimal % 2;
-            decimal / 2;
+            decimal = decimal / 2;
             j++;
-         }
-    }
+        }
 
-    // print bulbs
-
-    for (int k = (BITS_IN_BYTE - 1); k >= 0; k--)
-    {
-        print_bulb(binary[k]);
+        // print bulbs
+        for (int k = (BITS_IN_BYTE - 1); k >= 0; k--)
+        {
+            print_bulb(binary[k]);
+        }
+        printf("\n");
     }
-    printf("/n");
 }
 
 
