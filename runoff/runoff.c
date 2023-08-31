8992b5857773697fa7696e47516c28fc206458e4
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-08-31 15:46:22 +0000

    Thu, Aug 31, 2023, 11:46 PM +08:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -128,6 +128,10 @@ int main(int argc, string argv[])
 bool vote(int voter, int rank, string name)
 {
     // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp/9)
+    }
     return false;
 }
 
