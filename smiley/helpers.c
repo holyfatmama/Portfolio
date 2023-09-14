9680cb194ea4ad15313ecdd209715f196cc5292c
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-09-14 10:03:26 +0000

    Thu, Sep 14, 2023, 6:03 PM +08:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -3,4 +3,9 @@
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     // Change all black pixels to a color of your choosing
+
+    for (int h = 0; h < height; h++)
+    {
+        for (int w = 0; w < width; w++)
+    }
 }
