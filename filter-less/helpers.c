de557f865bd550575ac19b856f7f50e89e98f413
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-09-15 09:08:51 +0000

    Fri, Sep 15, 2023, 5:08 PM +08:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -7,17 +7,17 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int w = 0; w < width; w++)
         {
-            {
                 float red = image[h][w].rgbtRed;
                 float blue = image[h][w].rgbtBlue;
                 float green = image[h][w].rgbtGreen;
 
                 int average = round ((red + blue + green)/3);
 
-                image[h][w].rgbtRed = average;
+            if (image[h][w].rgbtRed != image[h][w].rgbtBlue && image[h][w].rgbtRed != image[h][w].rgbtGreen && image[h][w].rgbtBlue != image[h][w].rgbtGreen)
+            }
+              image[h][w].rgbtRed = average;
                 image[h][w].rgbtBlue = average;
                 image[h][w].rgbtGreen = average;
-            }
         }
     }
     return;
