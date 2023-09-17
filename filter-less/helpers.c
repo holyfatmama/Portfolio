ab95438a8861e19758d2bb1f3ad66faf2efdb6c8
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-09-17 12:09:33 +0000

    Sun, Sep 17, 2023, 8:09 PM +08:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -83,10 +83,12 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             int tempBlue = ((image[i-1][j-1].rgbtBlue + image[i-1][j].rgbtBlue + image[i-1][j+1].rgbtBlue + image[i][j-1].rgbtBlue + image[i][j].rgbtBlue + image[i][j+1].rgbtBlue + image[i+1][j-1].rgbtBlue + image[i+1][j].rgbtBlue + image[i+1][j+1].rgbtBlue)/9);
             int tempGreen = ((image[i-1][j-1].rgbtGreen + image[i-1][j].rgbtGreen + image[i-1][j+1].rgbtGreen + image[i][j-1].rgbtGreen + image[i][j].rgbtGreen + image[i][j+1].rgbtGreen + image[i+1][j-1].rgbtGreen + image[i+1][j].rgbtGreen + image[i+1][j+1].rgbtGreen)/9);
 
-            RGBTRIPLE temp = image[i][j];
-            temp.rgbtRed = tempRed;
-            temp.rgbtBlue = tempBlue;
-            temp.rgbtGreen = tempGreen;
+            int x = i;
+            int y = j;
+            RGBTRIPLE temp[x][y] = image[i][j];
+            temp[x][y].rgbtRed = tempRed;
+            temp[x][y].rgbtBlue = tempBlue;
+            temp[x][y].rgbtGreen = tempGreen;
 
             image[i][j].rgbtRed = tempRed;
             image[i][j].rgbtBlue = tempBlue;
