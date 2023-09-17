074ec9a894b9276766c35a4701779e80338f3142
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-09-17 11:28:14 +0000

    Sun, Sep 17, 2023, 7:28 PM +08:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -75,6 +75,12 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0 < j < width; j++)
+        {
+            RGBTRIPLE temp = image[i][j]
+        }
+    }
     return;
 }
