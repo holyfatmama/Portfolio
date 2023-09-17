a358a8a0b69a565277923aa45c7c895bf89dd118
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-09-17 11:24:56 +0000

    Sun, Sep 17, 2023, 7:24 PM +08:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -60,14 +60,13 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    int reflect = (width/2);
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < reflect; j++)
+        for (int j = 0; j < width/2; j++)
         {
             RGBTRIPLE temp = image[i][j];
             image[i][j] = image[i][width - (j + 1)];
-            image[i][width - (j + 1)] = image[i][j];
+            image[i][width - (j + 1)] = temp;
         }
     }
     return;
