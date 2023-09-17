7a59256bce058e76d5c132081206b166f94724b1
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-09-17 11:01:45 +0000

    Sun, Sep 17, 2023, 7:01 PM +08:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -65,7 +65,8 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int w = 0; w < reflect; w++)
         {
-            int temp = image[h][w];
+            int temp = array[i][i]
+            array[i][i] = image[h][w];
             image[h][w] = image[h][width - (w + 1)];
             image[h][width - (w+1)] = image[x][x];
         }
