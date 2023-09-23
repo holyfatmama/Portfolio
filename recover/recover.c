80c7e357a7a3efd6a55059ecad0965d805ae3b69
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-09-23 13:55:45 +0000

    Sat, Sep 23, 2023, 9:55 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -39,8 +39,9 @@ int main(int argc, char *argv[])
         // check if bytes are the start of a jpeg file
         if (buffer [0] == 0xff && buffer [1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xef)
         {
-            fwrite()
+            sprintf(output_file, "%3i.jpg", image_count);
         }
+
     }
 
 
