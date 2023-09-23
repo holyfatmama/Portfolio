10917aff0767f4fab0aa87d48b337341d7a9296c
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-09-23 06:01:07 +0000

    Sat, Sep 23, 2023, 2:01 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    // if more than one argument, ask for another one
+
+    // open memory card
+
+    // look for jpeg file
+
+    // write 512 bytes until new jpeg file is found
+
 }
