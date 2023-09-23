b84eff049ed6319d3f8435411df3a867679d9fff
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-09-23 13:57:37 +0000

    Sat, Sep 23, 2023, 9:57 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -40,6 +40,8 @@ int main(int argc, char *argv[])
         if (buffer [0] == 0xff && buffer [1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xef)
         {
             sprintf(output_file, "%3i.jpg", image_count);
+            fopen(output_file, "w");
+            fwrite(buffer, sizeof(char), 512,)
         }
 
     }
