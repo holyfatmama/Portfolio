7929bdedeb112334acf416d01142fe02f98beab5
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-09-28 16:10:45 +0000

    Fri, Sep 29, 2023, 12:10 AM +08:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -50,8 +50,8 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        p->parent[0] = parent0;
-        p->parent[1] = parent1;
+        p->parents[0] = parent0;
+        p->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
         p->alleles[0] = person *parent0.alleles[rand ()%2];
@@ -63,8 +63,8 @@ person *create_family(int generations)
     else
     {
         // TODO: Set parent pointers to NULL
-        p->parent[0] == NULL;
-        p->parent[1] == NULL;
+        p->parents[0] == NULL;
+        p->parents[1] == NULL;
 
         // TODO: Randomly assign alleles
         p->alleles[0] == rand () %2;
