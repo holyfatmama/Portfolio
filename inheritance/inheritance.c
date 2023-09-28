e9c6a0c847a942a9bdb17c03b2b2c92a6f5004f9
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-09-28 16:14:38 +0000

    Fri, Sep 29, 2023, 12:14 AM +08:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -63,8 +63,8 @@ person *create_family(int generations)
     else
     {
         // TODO: Set parent pointers to NULL
-        p->parents[0] == NULL;
-        p->parents[1] == NULL;
+        parent0 == NULL;
+        parent1 == NULL;
 
         // TODO: Randomly assign alleles
         p->alleles[0] == rand () %2;
