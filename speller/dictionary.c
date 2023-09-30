c194b445c03abe996ed1441ff13adccf3ff05b21
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-09-30 17:20:45 +0000

    Sun, Oct 1, 2023, 1:20 AM +08:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -19,7 +19,8 @@ const unsigned int N = 26;
 // Hash table
 node *table[N];
 
-// Returns true if word is in dictionary, else false
+// Returns true if word is in dictionary
+//, else false
 bool check(const char *word)
 {
     // TODO
@@ -29,25 +30,29 @@ bool check(const char *word)
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
-    // TODO: Improve this hash function, use some sort of for loop, A is a char (ascii)
+    // TODO: Improve this hash function
+    //, use some sort of for loop, A is a char (ascii)
     return toupper(word[0]) - 'A';
 }
 
-// Loads dictionary into memory, returning true if successful, else false, use malloc
+// Loads dictionary into memory, returning true if successful, else false
+//, use malloc
 bool load(const char *dictionary)
 {
     // TODO
     return false;
 }
 
-// Returns number of words in dictionary if loaded, else 0 if not yet loaded (use printf and print number of words)
+// Returns number of words in dictionary if loaded, else 0 if not yet loaded
+//(use printf and print number of words)
 unsigned int size(void)
 {
     // TODO
     return 0;
 }
 
-// Unloads dictionary from memory, returning true if successful, else false (use free memory)
+// Unloads dictionary from memory, returning true if successful, else false
+//(use free memory)
 bool unload(void)
 {
     // TODO
