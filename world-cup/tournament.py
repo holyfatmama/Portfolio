3213184f792e39a7a3ddc04894cb87187435a2f2
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-10-06 09:14:48 +0000

    Fri, Oct 6, 2023, 5:14 PM +08:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -17,7 +17,7 @@ def main():
     teams = []
     # TODO: Read teams into memory from file
         f = open("2018m.csv", "r")
-        csv.reader(f, delimter="")
+        csv.reader(f)
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
