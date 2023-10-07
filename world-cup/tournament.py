ca6abc0e42680fbb9f9cf685f16b8e144636cb18
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-10-07 19:12:38 +0000

    Sun, Oct 8, 2023, 3:12 AM +08:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -65,7 +65,7 @@ def simulate_round(teams):
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     # TODO
-    for len(teams) > 1:
+    while teams > 1:
         teams = simulate_round(teams)
     return teams[0]["team"]
 
