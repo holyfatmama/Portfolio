5adaad26953ceeef5c76e7c84e94ef8bbf89a6d4
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-10-08 12:33:48 +0000

    Sun, Oct 8, 2023, 8:33 PM +08:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,13 +1,14 @@
 # TODO
 from cs50 import get_int
 
-
+# get height of tower from user
 while True:
       height = get_int("height of tower?")
       if height > 0 & height < 9:
             break
 
-width = height - 1
-
-for i in range(height):
-    print("#", end="")
+#print height 
+h = 0
+while h < height:
+      print("#")
+      h += 1
