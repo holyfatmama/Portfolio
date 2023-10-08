d6f73ca5fc6c42c2b5a653e1b46063d20d16309f
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-10-08 15:03:05 +0000

    Sun, Oct 8, 2023, 11:03 PM +08:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -2,39 +2,40 @@
 from cs50 import get_float
 
 
-# get amount of cents from user
+#get amount of cents from user
 while True:
-    dollars = get_float("How much change?\n")
+    dollars = get_float("how many dollars\n")
     if dollars > 0:
         break
 
-cents = round(dollars * 100)
-
 # quarters
 quarters = 0
-while cents >= 25:
-    cents = cents - 25
-    quarters += 1
+while (dollars >= 0.25):
+        dollars = dollars - 0.25
+        quarters += 1
 
 # dime
 dime = 0
-while cents >= 10:
-    cents = cents - 10
-    dime += 1
+while (dollars >= 0.10):
+     dollars = dollars - 0.10
+     dime += 1
 
 
 # nickel
 nickel = 0
-while cents >= 5:
-    cents = cents - 5
-    nickel += 1
+while (dollars >= 0.05):
+     dollars = dollars - 0.05
+     nickel += 1
 
 # pennies
 pennies = 0
-while cents >= 1:
-    cents = cents - 1
-    pennies += 1
+
+while (dollars >= 0.01):
+     dollars = dollars - 0.01
+     pennies += 1
 
 coins = quarters + dime + nickel + pennies
 
 print(coins)
+
+
