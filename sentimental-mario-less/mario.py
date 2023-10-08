0b90f5d806b51a8fb5654cf8ec364c0a3dd2bf54
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-10-08 12:20:27 +0000

    Sun, Oct 8, 2023, 8:20 PM +08:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -3,8 +3,12 @@ from cs50 import get_int
 
 height = get_int("height of tower?")
 
-while height > 8 or < 1:
-      
+while true:
+      height = get_int("height of tower?")
+      if height > 8 or < 1
+            break
+      return height
+
 width = height - 1
 
 for i in range(height):
