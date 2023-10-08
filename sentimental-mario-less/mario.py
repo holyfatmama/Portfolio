00562916caf58f279145d54d273c8c1038896475
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-10-08 11:18:28 +0000

    Sun, Oct 8, 2023, 7:18 PM +08:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,10 +1,10 @@
 # TODO
 
 height = input("Whats the height")
-while height >8 or <1:
+while height > 8:
     height = input("Whats the height")
     width = int(height) - 1
 
 for i in range(height):
     for j in range(width):
-    print("#")
+            print("#")
