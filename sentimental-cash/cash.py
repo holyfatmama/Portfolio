cc133cf13cf5192c2ba902c936fcd188fd485bf2
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-10-08 13:44:59 +0000

    Sun, Oct 8, 2023, 9:44 PM +08:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,13 +1,21 @@
 # TODO
+from cs50 import get_float
 from cs50 import get_int
 
 #get amount of cents from user
 
-cents = get_int("how many cents")
+while True:
+    cents = get_int("how many dollars")
+    if cents > 0:
+        break
 
 # quarters
 quarters = 0
-while (cents >= 25):
-    cents = cents - 25
-    quarters += 1
+while True:
+    while (cents >= 25):
+        cents = cents - 25
+        quarters += 1
+        break
+
+print(quarters)
 
