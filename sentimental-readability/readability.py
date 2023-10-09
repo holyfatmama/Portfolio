81408f0b0e87607169aee9ad842552499e97a3c4
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-10-09 02:03:39 +0000

    Mon, Oct 9, 2023, 10:03 AM +08:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -5,23 +5,23 @@ input = get_string("Text: ")
 
 letters = 0
 for i in range(len(input)):
-    if (str.isalpha(input[i]) == True):
+    if str.isalpha(input[i]) == True:
         letters += 1
-    elif (str.isspace(input[i]) == True):
+    elif str.isspace(input[i]) == True:
         letters - 1
 
 print(f"Letters: {letters}")
 
 words = 1
 for i in range(len(input)):
-    if (str.isspace(input[i]) == True):
+    if str.isspace(input[i]) == True:
         words += 1
 
 print(f"Words: {words}")
 
 sentences = 0
 for i in range(len(input)):
-    if (input[i] == "." or input [i] == "?" or input[i] == "!"):
+    if input[i] == "." or input[i] == "?" or input[i] == "!":
         sentences += 1
 
 print(f"Sentences: {sentences}")
@@ -31,9 +31,9 @@ S = sentences / words * 100
 
 index = round(0.0588 * L - 0.296 * S - 15.8)
 
-if (index < 1):
+if index < 1:
     print("Before Grade 1")
-elif (index > 16):
+elif index > 16:
     print("Grade 16+")
 else:
     print(f"Grade: {index}")
