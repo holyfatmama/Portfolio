1ed1977069bdc1024dbcd676ad07c65f8882088f
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-10-09 12:29:47 +0000

    Mon, Oct 9, 2023, 8:29 PM +08:00

--- dna/dna.py
+++ dna/dna.py
@@ -11,7 +11,7 @@ def main():
     # TODO: Read database file into a variable
     filename = sys.argv[1]
     f = open(filename)
-    
+     
     # TODO: Read DNA sequence file into a variable
 
     # TODO: Find longest match of each STR in DNA sequence
