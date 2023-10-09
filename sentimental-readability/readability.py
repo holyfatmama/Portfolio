668b688c37603604f833c31444f52a6c5778f4c8
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-10-09 01:32:30 +0000

    Mon, Oct 9, 2023, 9:32 AM +08:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -5,9 +5,9 @@ input = get_string("Text: ")
 
 letters = 0
 for i in range(len(input)):
-    letter = str.isalpha(input[i-1])
-    letters += 1
-    space = str.isletter(input[i-1])
-    
+    if (str.isalpha(input[i-1]) == True):
+        letters += 1
+    elif (str.isspace(input[i-1]) == True):
+        letters - 1
 
 print(letters)
