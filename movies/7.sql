f3fb7e5a414d65b78fc2418d5cf5938024c55970
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-10-14 15:27:27 +0000

    Sat, Oct 14, 2023, 11:27 PM +08:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,3 +1,3 @@
 SELECT title FROM movies WHERE year = '2010';
 
-SELECT rating FROM ratings WHERE movie_id = (SELECT id FROM movies WHERE year = '2010');
+SELECT rating FROM ratings WHERE movie_id LIKE (SELECT id FROM movies WHERE year = '2010');
