dc9f6c817aa33da2129f72c06f42ac5cb340b49f
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-10-14 15:01:52 +0000

    Sat, Oct 14, 2023, 11:01 PM +08:00

--- movies/6.sql
+++ movies/6.sql
@@ -1,1 +1,1 @@
-SELECT AVG(rating) FROM ratings WHERE movie_id = (SELECT movie_id FROM movies WHERE year = '2012')
+SELECT AVG(rating) FROM ratings WHERE movie_id = (SELECT movie_id FROM movies WHERE year = '2012');
