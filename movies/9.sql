58b410c9af4ca6362c1ab02479e0d78fb5375f21
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-10-14 16:41:50 +0000

    Sun, Oct 15, 2023, 12:41 AM +08:00

--- movies/9.sql
+++ movies/9.sql
@@ -2,4 +2,4 @@ movies, 2004
 people, names
 stars
 
-SELECT name FROM people WHERE id 
+SELECT name FROM people WHERE id IN (SELECT person_id FROM stars WHERE movie_id IN (SELECT movie_id FROM movies WHERE year = '2004'));
