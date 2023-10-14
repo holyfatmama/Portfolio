0d2350776ad07a1c0b39a71bb1b2da7d20d47877
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-10-14 17:29:26 +0000

    Sun, Oct 15, 2023, 1:29 AM +08:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,1 +1,4 @@
 SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = 'Bradley Cooper' OR name = 'Jennifer Lawrence');
+
+SELECT id FROM people WHERE name = 'Bradley Cooper';
+SELECT id FROM people WHERE name = 'Jennifer Lawrence';
