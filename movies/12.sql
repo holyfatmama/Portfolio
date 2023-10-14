c70e4d0756a73a902605a062cd97ab29b451a64e
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-10-14 17:41:17 +0000

    Sun, Oct 15, 2023, 1:41 AM +08:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,13 +1,1 @@
-SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = 'Bradley Cooper' OR name = 'Jennifer Lawrence');
-
-SELECT id FROM people WHERE name = 'Bradley Cooper';
-SELECT id FROM people WHERE name = 'Jennifer Lawrence';
-
-movies that bradley starred in
-SELECT * FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Bradley Cooper');
-
-movies that jlaw starred in
-SELECT * FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Jennifer Lawrence');
-
-'
-SELECT movie_id FROM stars JOIN people ON id = person_id WHERE name = 'Jennifer Lawrence' OR name = 'Bradley Cooper' GROUP BY movie_ID HAVING COUNT(movie_id) > 1;
+SELECT title FROM movies WHERE id IN (SELECT movie_id FROM stars JOIN people ON id = person_id WHERE name = 'Jennifer Lawrence' OR name = 'Bradley Cooper' GROUP BY movie_ID HAVING COUNT(movie_id) > 1);
