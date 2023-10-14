ca65c4663fb13caf91979e441e8a4cc573c431cf
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-10-14 16:52:26 +0000

    Sun, Oct 15, 2023, 12:52 AM +08:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,1 +1,1 @@
-SELECT name FROM people JOIN stars ON person_id = id WHERE movie_id IN (SELECT id FROM movies WHERE year = '2004') ORDER BY birth ASC;
+SELECT DISTINCT(name) FROM people JOIN stars ON person_id = id WHERE movie_id IN (SELECT id FROM movies WHERE year = '2004') ORDER BY birth ASC;
