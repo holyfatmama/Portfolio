5b4cdd394086673ac458a071700799c58ab003c7
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-10-15 13:03:01 +0000

    Sun, Oct 15, 2023, 9:03 PM +08:00

--- movies/13.sql
+++ movies/13.sql
@@ -5,3 +5,5 @@ klevin bacon movcies
 SELECT * FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Kevin Bacon' AND birth = '1958');
 
 SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Kevin Bacon' AND birth = '1958');
+
+SELECT * FROM stars WHERE movie_id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Kevin Bacon' AND birth = '1958'));
