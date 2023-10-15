e30af63364ab4a1713eab94874d7822ad73a55f0
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-10-15 15:19:58 +0000

    Sun, Oct 15, 2023, 11:19 PM +08:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,1 +1,3 @@
-SELECT DISTINCT(name) FROM directors JOIN people ON id = person_id WHERE movie_id IN (SELECT movie_id FROM ratings WHERE rating > '9.0');
+SELECT DISTINCT(name) FROM directors JOIN people ON id = person_id WHERE movie_id IN (SELECT movie_id FROM ratings WHERE rating >= '9.0');
+
+
--- movies/11.sql
+++ movies/11.sql
@@ -1,1 +1,1 @@
-SELECT title FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id JOIN ratings ON ratings.movie_id = stars.movie_id WHERE name = 'Chadwick Boseman' ORDER BY rating DESC;
+SELECT title FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id JOIN ratings ON ratings.movie_id = stars.movie_id WHERE name = 'Chadwick Boseman' ORDER BY rating DESC LIMIT 5;
