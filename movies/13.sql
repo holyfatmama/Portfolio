2fffdca7718911cbca08a4c8aa36b7afc7c8d8d5
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-10-15 13:06:28 +0000

    Sun, Oct 15, 2023, 9:06 PM +08:00

--- movies/13.sql
+++ movies/13.sql
@@ -6,6 +6,6 @@ SELECT * FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Kevin
 
 SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Kevin Bacon' AND birth = '1958');
 
-SELECT * FROM stars WHERE movie_id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Kevin Bacon' AND birth = '1958'));
+movies that have kevin bacon and other stars
+SELECT person_id FROM stars WHERE movie_id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Kevin Bacon' AND birth = '1958'));
 
-SELECT * FROM movies WHERE id IN (SELECT movie_id FROM stars WHERE movie_id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Kevin Bacon' AND birth = '1958')));
