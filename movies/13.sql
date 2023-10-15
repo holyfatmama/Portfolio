35ce3b6af4d1606742a95b19630fdfa2996f1910
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-10-15 13:09:58 +0000

    Sun, Oct 15, 2023, 9:09 PM +08:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,11 +1,1 @@
-keving bacon id
-SELECT id FROM people WHERE name = 'Kevin Bacon' AND birth = '1958';
-
-klevin bacon movcies
-SELECT * FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Kevin Bacon' AND birth = '1958');
-
-SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Kevin Bacon' AND birth = '1958');
-
-movies that have kevin bacon and other stars
-SELECT person_id FROM stars WHERE movie_id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Kevin Bacon' AND birth = '1958'));
-
+SELECT name FROM people WHERE id IN (SELECT person_id FROM stars WHERE movie_id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Kevin Bacon' AND birth = '1958'))) AND name != 'Kevin Bacon' ORDER BY name ASC;
