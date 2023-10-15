979f06b308828e952ee806822a506c55fee9346f
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-10-15 13:00:13 +0000

    Sun, Oct 15, 2023, 9:00 PM +08:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,3 +1,5 @@
 keving bacon id
-SELECT * FROM people WHERE name = 'Kevin Bacon' AND birth = '1958';
+SELECT id FROM people WHERE name = 'Kevin Bacon' AND birth = '1958';
 
+klevin bacon movcies
+SELECT * FROM stars WHERE person_id = (SELECT id FROM people WHERE name = 'Kevin Bacon' AND birth = '1958');
