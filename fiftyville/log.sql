17f9343a0ebd68a144e037a1d4646f96d71bdec1
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-10-16 15:28:26 +0000

    Mon, Oct 16, 2023, 11:28 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -14,7 +14,11 @@ id 163, Lily
 | 162 | Eugene  | 2021 | 7     | 28  | I don't know the thief's name, but it was someone I recognized. Earlier this morning, before I arrived at Emma's bakery, I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money.                                                                                                 |
 | 163 | Raymond | 2021 | 7     | 28  | As the thief was leaving the bakery, they called someone who talked to them for less than a minute. In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow. The thief then asked the person on the other end of the phone to purchase the flight ticket. |
 
+Car, parking lot, exit by 1025am, 28 July
+
 161 Ruth
 SELECT * FROM bakery_security_logs WHERE month = '7' AND day = '28' AND hour = '10' AND activity = 'exit' AND minute <= '25';
 
 162 Eugene
+ATM, Leggett Street, withdraw
+SELECT * FROM atm_transactions WHERE month = '7' AND day = '28' AND atm_location = 'Leggett Street' And transaction_type = 'withdraw';
