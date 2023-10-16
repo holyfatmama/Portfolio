a7e375e419cf8b8973cd6c745468ea13a612edde
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-10-16 15:48:40 +0000

    Mon, Oct 16, 2023, 11:48 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -45,3 +45,5 @@ SELECT * FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE
 
 suspect list from flight
 SELECT * FROM people WHERE passport_number IN (SELECT passport_number FROM passengers WHERE flight_id = 36);
+
+SELECT * FROM people WHERE phone_number IN (SELECT phone_number FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE month = '7' AND day = '28' AND duration <= '60' ORDER BY duration ASC) AND passport_number IN (SELECT passport_number FROM people WHERE passport_number IN (SELECT passport_number FROM passengers WHERE flight_id = 36)) AND license_plate = (SELECT license_plate FROM people WHERE passport_number IN (SELECT passport_number FROM passengers WHERE flight_id = 36));
