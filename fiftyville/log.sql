09edb6bd3a5ddbe735caf6d333250ecc7d1158c3
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-10-16 15:23:12 +0000

    Mon, Oct 16, 2023, 11:23 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,6 +1,8 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 SELECT * FROM crime_scene_reports WHERE month = '7' AND day = '28' AND street = 'Humphrey Street';
 
+time of crime: 1015am
+
 id of crime scene report = 295
 
 SELECT * FROM interviews WHERE month = '7' AND day = '28';
