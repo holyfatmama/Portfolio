268e2c3c318b14f0c2423830f2d93fb80eef444f
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-11-01 14:27:28 +0000

    Wed, Nov 1, 2023, 10:27 PM +08:00

--- random.py
+++ random.py
@@ -1,4 +1,14 @@
 stock = [{"name": "jun"}, {"name": "han"}]
 
+stock2 = {}
 stock2["name"] = stock[0]["name"]
 print(stock2["name"])
+print(stock2)
+
+for sto in stock:
+    print(sto)
+
+shares = [{"symbol" : "aapl", "SUM(shares)" : "4"}, {"symbol" : "tsla", "SUM(shares)" : "6"}]
+
+for share in shares:
+    
