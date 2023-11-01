eeee1b4498ddecc6dda775ca9f40eda05171bf53
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-11-01 14:33:46 +0000

    Wed, Nov 1, 2023, 10:33 PM +08:00

--- random.py
+++ random.py
@@ -8,7 +8,7 @@ print(stock2)
 for sto in stock:
     print(sto)
 
-shares = [{"symbol" : "aapl", "SUM(shares)" : "4"}, {"symbol" : "tsla", "SUM(shares)" : "6"}, {"value" : "4"}]
+shares = [{"symbol" : "aapl", "SUM(shares)" : "4"}, {"symbol" : "tsla", "SUM(shares)" : "6"}]
 
 
 print(shares[2])
