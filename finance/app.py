69e11251a3e4ca83a8786b78164cdb1e8e1afb17
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-11-04 18:55:55 +0000

    Sun, Nov 5, 2023, 2:55 AM +08:00

--- finance/app.py
+++ finance/app.py
@@ -100,7 +100,7 @@ def buy():
 def history():
     """Show history of transactions"""
     # look through each line of transaction where ? equals to username and display it
-    transactions = db.execute("SELECT * FROM transactions WHERE user_id = ?", session["user_id"])
+    transactions = db.execute("SELECT * FROM transactions WHERE user_id = ? ORDER BY timestamp DESC", session["user_id"])
 
     return render_template("history.html",  transactions = transactions)
 
