eaf451ffe68443e2c3bc6d9aa398132d64e86a26
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-11-08 16:31:45 +0000

    Thu, Nov 9, 2023, 12:31 AM +08:00

--- project/app.py
+++ project/app.py
@@ -1,6 +1,9 @@
 import os
 
 from cs50 import SQL
-from flash import render_template, redirect, Flask
+from flask import render_template, redirect, Flask
 
 app = Flask(__name__)
+
+db = SQL("sqlite:///tasks.db")
+
