4cf5777853cbd88e677f6e76944dd76831cf5980
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-11-10 14:55:57 +0000

    Fri, Nov 10, 2023, 10:55 PM +08:00

--- project/app.py
+++ project/app.py
@@ -14,10 +14,9 @@ def index():
     if request.method == "POST":
         id = request.form.get("id")
         db.execute("DELETE FROM tasks WHERE id = ?", id)
-        time.sleep(2)
         return render_template ("index.html", tasks = tasks)
+
     else:
-        time.sleep(2)
         return render_template("index.html", tasks = tasks)
 
 @app.route("/addtask", methods=["GET", "POST"])
@@ -31,8 +30,8 @@ def addtask():
         if not importance:
             importance = 0
         db.execute("INSERT INTO tasks (task, detail, importance, deadline) VALUES (?, ?, ?, ?)", task, detail, importance, deadline)
-        time.sleep(2)
+        time.sleep(1)
         return render_template("index.html", tasks = tasks)
     else:
-        time.sleep(2)
+        time.sleep(1)
         return render_template("addtask.html", tasks = tasks)
