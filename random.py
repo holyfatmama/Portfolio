b21a40e0d6208953aac5b9fa72c675bd7b492cce
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-11-26 15:39:31 +0000

    Sun, Nov 26, 2023, 11:39 PM +08:00

--- random.py
+++ random.py
@@ -10,6 +10,7 @@ for sto in stock:
 
 shares = [{"symbol" : "aapl", "SUM(shares)" : 4}, {"symbol" : "tsla", "SUM(shares)" : 6}]
 share = {"gay":"yes"}
+share
 print(share)
 share["values"] = shares[0]["SUM(shares)"] + shares[1]["SUM(shares)"]
 print("break")
