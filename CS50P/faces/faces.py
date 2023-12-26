749f65c656b28281b04cb60cd58102d2a14fc1ed
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-12-26 06:55:35 +0000

    Tue, Dec 26, 2023, 2:55 PM +08:00

--- CS50P/faces/faces.py
+++ CS50P/faces/faces.py
@@ -2,11 +2,10 @@ def main():
     message = input()
     face = convert(message)
     print(face)
-    return
 
 def convert(x):
     y = x.replace(":)","🙂")
-    y = x.replace(":(","🙁")
-    return y
+    z = y.replace(":(","🙁")
+    return z
 
 main()
