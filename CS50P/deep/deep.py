3333d4915094fa46edb11b48814d917f6c995f85
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-12-30 08:53:17 +0000

    Sat, Dec 30, 2023, 4:53 PM +08:00

--- CS50P/deep/deep.py
+++ CS50P/deep/deep.py
@@ -2,7 +2,7 @@ x = str(input("What is the Answer to the Great Question of Life, the Universe, a
 
 answer = x.strip().lower()
 
-if answer == "forty-two" or "forty two" or "42":
+if answer == "forty-two" or answer == "forty two" or answer == "42":
     print("Yes")
 else:
     print("No")
