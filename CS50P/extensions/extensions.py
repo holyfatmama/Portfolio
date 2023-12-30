e7d4cfa8f31da12780a8cce4b349b95b46fccc0a
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-12-30 10:31:29 +0000

    Sat, Dec 30, 2023, 6:31 PM +08:00

--- CS50P/extensions/extensions.py
+++ CS50P/extensions/extensions.py
@@ -1,5 +1,18 @@
 file_name = input("File name: ")
 
 if file_name.endswith(".gif"):
-    print()
-elif
+    print("image/jpeg")
+elif file_name.endswith(".jpg"):
+    print("image/jpeg")
+elif file_name.endswith(".jpeg"):
+    print("image/jpeg")
+elif file_name.endswith(".png"):
+    print("image/png")
+elif file_name.endswith(".pdf"):
+    print("application/pdf")
+elif file_name.endswith(".txt"):
+    print("text/plain")
+elif file_name.endswith(".zip"):
+    print("application/zip")
+else:
+    print("application/octet-stream")
