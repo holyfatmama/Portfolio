2f0299674a148a61abf6763eb66f6adb0cb0dbf3
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-12-30 10:27:08 +0000

    Sat, Dec 30, 2023, 6:27 PM +08:00

--- CS50P/extensions/extensions.py
+++ CS50P/extensions/extensions.py
@@ -3,7 +3,7 @@ file_name = input("File name: ")
 types = {".gif":"image/gif", ".jpg":"image/jpeg", ".jpeg":"image/jpeg", ".png":"image/png", ".pdf":"application/pdf", ".txt":"text/plain", ".zip":"application/zip"}
 
 for type in types:
-    if file_name.endswith(type):
-        print(types[type])
+    if file_name.endswith(type) in types:
+        print("exist")
     else:
-        continue
+        print("doesnt exist")
