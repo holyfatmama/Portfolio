12c93c2ea74d5606430fa6343b61817f808fbcac
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-12-30 09:45:37 +0000

    Sat, Dec 30, 2023, 5:45 PM +08:00

--- CS50P/bank/bank.py
+++ CS50P/bank/bank.py
@@ -3,7 +3,7 @@ x = input("Greeting: ")
 answer = x.strip().lower().split()[0]
 first_letter_answer = answer[0]
 
-if answer == "hello":
+if answer[0:5] == "hello":
     print("$0")
 elif first_letter_answer == "h":
     print("$20")
