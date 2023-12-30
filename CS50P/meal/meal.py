58075492f565cbeae05cb7183112ed8994cda861
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-12-30 11:06:10 +0000

    Sat, Dec 30, 2023, 7:06 PM +08:00

--- CS50P/meal/meal.py
+++ CS50P/meal/meal.py
@@ -1,4 +1,15 @@
 def main():
+    if hours >= 7 and hours <= 8:
+        print("breakfast time")
+    elif hours >= 12 and hours <=13:
+        print("lunch time")
+    elif hours >= 18 and hours <= 19:
+        print("dinner time")
 
 def convert(time):
-    
+    hours, minutes = time.split(":")
+    minutes = minutes / 60
+    return hours, minutes
+
+if __name__ == "__main__":
+    main()
