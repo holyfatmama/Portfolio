8bf663137e1a28e83f7add680daaac10824623fb
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-12-30 11:30:17 +0000

    Sat, Dec 30, 2023, 7:30 PM +08:00

--- CS50P/meal/meal.py
+++ CS50P/meal/meal.py
@@ -1,7 +1,6 @@
 def main():
     time = input("What time is it? ")
     answer = convert(time)
-    print(answer)
     if answer >= 7 and answer <= 8:
         print("breakfast time")
     elif answer >= 12 and answer <= 13:
@@ -11,8 +10,6 @@ def main():
 
 def convert(time):
     hours, minutes = time.split(":")
-    print(hours)
-    print(minutes)
     hours = float(hours)
     minutes = float(minutes)
     minutes = minutes / 60
