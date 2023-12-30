80788dc86d308de7ed46104c78695a2db70c3eeb
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-12-30 10:50:04 +0000

    Sat, Dec 30, 2023, 6:50 PM +08:00

--- CS50P/interpreter/interpreter.py
+++ CS50P/interpreter/interpreter.py
@@ -1,11 +1,10 @@
 x, y, z= input("Expression: ").split(" ")
 
-print(int(x) y int(z))
-
 if y == "+":
     print(int(x) + int(z))
 elif y == "-":
     print(int(x) - int(z))
 elif y == "*":
-    print(int)
-
+    print(int(x) * int(z))
+elif y == "/":
+    print(int(x) / int(z))
