ba1c2ffb933347432c68f46fcc6e168d47513a36
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2023-12-30 10:10:08 +0000

    Sat, Dec 30, 2023, 6:10 PM +08:00

--- CS50P/extensions/extensions.py
+++ CS50P/extensions/extensions.py
@@ -1,6 +1,6 @@
 file_name = input("File name: ")
 
-types = {.gif:image/gif, .jpg:image/jpeg, .jpeg:image/jpeg, .png:image/png, .pdf:application/pdf, .txt:text/plain, .zip}
+types = {.gif:image/gif, .jpg:image/jpeg, .jpeg:image/jpeg, .png:image/png, .pdf:application/pdf, .txt:text/plain, .zip:application/zip}
 
 for type in types:
     if file_name.endswith(type):
