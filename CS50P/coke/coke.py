be1eb9c34a6e58f9520816a9c2b4c597f2fb045a
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2024-01-07 11:50:31 +0000

    Sun, Jan 7, 2024, 7:50 PM +08:00

--- CS50P/coke/coke.py
+++ CS50P/coke/coke.py
@@ -5,8 +5,10 @@ while amount_due > 0:
     x = int(input("Insert Coin:"))
     if x == 25 or x == 10 or x == 5:
         amount_due = amount_due - x
-    print(f"Amount Due: {amount_due}")
+    if amount_due < 0:
+        print(f"Change Owed: {abs(amount_due)}")
+    else:
+        print(f"Amount Due: {amount_due}")
 
-if amount_due < 0:
-    print(f"Change Owed: {abs(amount_due)}")
+
 
