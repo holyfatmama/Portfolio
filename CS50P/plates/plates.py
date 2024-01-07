e28940f38a5ce860314420c85e54009f763435ed
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2024-01-07 13:19:21 +0000

    Sun, Jan 7, 2024, 9:19 PM +08:00

--- CS50P/plates/plates.py
+++ CS50P/plates/plates.py
@@ -11,8 +11,6 @@ def is_valid(s):
         return False
     if len(s) < 2 and len(s) > 6:
         return False
-    if s[-1].isnumeric == True:
-        return False
     for i in s:
         if i.isalnum() == False:
             if i == '0':
