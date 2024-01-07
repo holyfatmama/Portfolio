0a27232187b8357277ae900c5c3ab602601df756
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2024-01-07 13:05:38 +0000

    Sun, Jan 7, 2024, 9:05 PM +08:00

--- CS50P/plates/plates.py
+++ CS50P/plates/plates.py
@@ -17,10 +17,9 @@ def is_valid(s):
         if i.isalnum() == False:
             if i == '0':
                 return False
-        else:
-            continue
     for c in s:
-        if c.
+        if c.isalnum() == False:
+            return False
     return True
 
 main()
