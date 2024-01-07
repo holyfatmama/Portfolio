71fed3abcd4094d2bb9d7d2822aca8a2c5fc1cb5
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2024-01-07 06:52:34 +0000

    Sun, Jan 7, 2024, 2:52 PM +08:00

--- CS50P/coke/coke.py
+++ CS50P/coke/coke.py
@@ -1,10 +1,8 @@
 amount_due = 50
 print(f"Amount Due: {amount_due}")
 
-x = int(input("Insert Coin:"))
-
-amount_due = amount_due - x
 while amount_due > 0:
     x = int(input("Insert Coin:"))
-    amount_due - x
-print(amount_due)
+    amount_due = amount_due - x
+    print(amount_due)
+
