284dbc3bb7507fa7bb22dd15e7045ce89d558f91
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2024-01-09 17:09:21 +0000

    Wed, Jan 10, 2024, 1:09 AM +08:00

--- CS50P/plates/plates.py
+++ CS50P/plates/plates.py
@@ -19,6 +19,9 @@ def is_valid(s):
                 return False
             else:
                 break
+    for i in range(len(s)-1):
+        if s[i].isdigit() and s[i+1].isalpha():
+            return False
     return True
 
 main()
