a911f89c2530774517ba892d91feaf9d9a9bdc3b
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2024-01-09 17:01:13 +0000

    Wed, Jan 10, 2024, 1:01 AM +08:00

--- CS50P/plates/plates.py
+++ CS50P/plates/plates.py
@@ -7,21 +7,10 @@ def main():
 
 
 def is_valid(s):
-    if s[0:2].isalpha() == False:
-        return False
     if len(s) < 2 or len(s) > 6:
         return False
-    for i in range(len(s)):
-        if s[i].isdigit() == True:
-            if int(s[i]) == 0:
-                return False
-        else:
-            break
-    for i in range(len(s)-1):
-        if s[i].isdigit() == True and s[i+1].isalpha() == True:
-            return False
-    if s.isalnum() == False:
+    if s[0:2].isdigit == True:
         return False
-    return True
+
 
 main()
