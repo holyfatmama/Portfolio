acd395391d481b3e3fdc9844109940a8a7ac8447
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2024-01-11 02:36:07 +0000

    Thu, Jan 11, 2024, 10:36 AM +08:00

--- CS50P/fuel/fuel.py
+++ CS50P/fuel/fuel.py
@@ -2,7 +2,10 @@ while True:
     try:
         xx = input("Fraction: ")
         x, y = xx.split("/")
-        output = ((int(x) / int (y)) * 100)
+        output = int(x) / int (y)
+        percentage = output * 100
+        if output > 1:
+            
         if output == 0:
             print("E")
             break
