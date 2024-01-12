413d1dffd8554f90b8780ece66c0a868aa969b83
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2024-01-12 06:49:08 +0000

    Fri, Jan 12, 2024, 2:49 PM +08:00

---
+++ CS50P/taqueria/taqueria.py
@@ -0,0 +1,20 @@
+prices = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
+item = input("Item: ").title()
+
+if item in prices:
+    total_price = 0
+    total_price += prices[item]
+
+
+
