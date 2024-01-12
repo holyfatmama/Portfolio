1ef0a17ef9775ef64b7716661b7d171e53e06725
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2024-01-12 08:02:45 +0000

    Fri, Jan 12, 2024, 4:02 PM +08:00

--- CS50P/grocery/grocery.py
+++ CS50P/grocery/grocery.py
@@ -1,16 +1,13 @@
 # dictionary (item:count)
-grocery_list = {
-    "apple":10
-}
+grocery_list = {}
 
 while True:
     try:
         item = input()
         if item in grocery_list:
-            grocery_list.update(item = "int(grocery_list[item]) + 1")
-            print(grocery_list[item])
+            grocery_list.update({item:int(grocery_list[item]) + 1})
         else:
-            grocery_list.update(item = 1)
+            grocery_list.update({item:1})
     except EOFError:
         for item in grocery_list:
             print(f"{grocery_list[item]} {item}")
