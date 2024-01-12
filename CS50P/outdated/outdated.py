3f0fd45b92a8a26d0cba6f53387e865df7c3c25d
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2024-01-12 08:42:57 +0000

    Fri, Jan 12, 2024, 4:42 PM +08:00

--- CS50P/outdated/outdated.py
+++ CS50P/outdated/outdated.py
@@ -18,7 +18,7 @@ x = input("Date: ")
 month, day, year = x.split("/", "")
 print(month, day, year)
 
-if int(month) > 12 or month not in months:
+if month not in months:
     print("error")
 if int(day) > 31:
     print("error")
