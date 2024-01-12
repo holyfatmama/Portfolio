d9d8d474168e32eb231d6ba83a4dbeb556abd56f
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2024-01-12 09:04:50 +0000

    Fri, Jan 12, 2024, 5:04 PM +08:00

--- CS50P/outdated/outdated.py
+++ CS50P/outdated/outdated.py
@@ -13,19 +13,4 @@ months = [
     "December"
 ]
 
-x = input("Date: ")
-
-if x[0].isnumeric:
-    month, day, year = x.split("/")
-    print(month, day, year)
-elif x[0]:
-    month, day, year = x.split("/")
-
-month, day, year = x.split("/")
-print(month, day, year)
-
-if month not in months:
-    print("error")
-if int(day) > 31:
-    print("error")
 
