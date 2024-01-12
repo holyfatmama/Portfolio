44f6ede7c9686ef3fad6ef89fc6e6702b235e17a
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2024-01-12 08:42:23 +0000

    Fri, Jan 12, 2024, 4:42 PM +08:00

--- CS50P/outdated/outdated.py
+++ CS50P/outdated/outdated.py
@@ -1,9 +1,24 @@
+months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
 x = input("Date: ")
 
 month, day, year = x.split("/", "")
 print(month, day, year)
 
-if int(month) > 12:
+if int(month) > 12 or month not in months:
     print("error")
 if int(day) > 31:
     print("error")
