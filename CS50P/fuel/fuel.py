c251bf42926aaec06d3aeaa7a29a011bea5aac25
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2024-01-12 06:26:12 +0000

    Fri, Jan 12, 2024, 2:26 PM +08:00

--- CS50P/fuel/fuel.py
+++ CS50P/fuel/fuel.py
@@ -4,14 +4,14 @@ while True:
         x, y = xx.split("/")
         output = int(x) / int (y)
         percentage = output * 100
-        if 0 <= output <= 1:
+        if 0 <= percentage <= 1:
             print("E")
             break
-        elif 99 <= output <= 100:
+        elif 99 <= percentage <= 100:
             print("F")
             break
         else:
-            print(f"{int(output)}%")
+            print(f"{int(percentage)}%")
             break
     except:
         print("Error")
