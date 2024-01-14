2dabf446abe1eea78034e309a8c18029bb0b25aa
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2024-01-14 13:27:31 +0000

    Sun, Jan 14, 2024, 9:27 PM +08:00

--- CS50P/outdated/outdated.py
+++ CS50P/outdated/outdated.py
@@ -20,11 +20,13 @@ try:
     if (1 <= int(month) <= 12) and (1 <= int(day) <= 31):
         print(year, month, day)
     else:
-        print("error")
+        print("error1")
 except:
     try:
         date = date.strip(",")
         month, day, year = date.split()
+        if month in months:
+            print(months.index(month) + 1)
         print(month)
     except:
-        print("error")
+        print("error2")
