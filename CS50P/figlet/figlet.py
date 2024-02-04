aa65e3a73ab2e3bb3318557a2da98c0c6747635c
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2024-02-04 12:46:50 +0000

    Sun, Feb 4, 2024, 8:46 PM +08:00

--- CS50P/figlet/figlet.py
+++ CS50P/figlet/figlet.py
@@ -5,9 +5,12 @@ import sys
 figlet = Figlet()
 x = input("heheL")
 
-if sys.argv[1] != "-f":
-    print("Invalid Usage")
-    exit()
+if len(sys.argv) == 1 or 3:
+    if sys.argv[1] != "-f":
+        print("Invalid Usage")
+        exit()
+    else:
+        figlet.setFont(font = sys.argv[2])
+        print(figlet.renderText(x))
 else:
-    figlet.setFont(font = sys.argv[2])
-    print(figlet.renderText(x))
+    exit()
