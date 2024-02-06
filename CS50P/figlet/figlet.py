0ef333c6650636305cc909561e6e6f1035a2e850
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2024-02-06 16:07:04 +0000

    Wed, Feb 7, 2024, 12:07 AM +08:00

--- CS50P/figlet/figlet.py
+++ CS50P/figlet/figlet.py
@@ -10,12 +10,12 @@ if len(sys.argv) == 1 or len(sys.argv) == 3:
     if len(sys.argv) == 3 and (sys.argv[1] == "-f" or sys.argv[1] == "--font"):
         f = sys.argv[2]
         figlet.setFont(font=f)
-        print(figlet.renderText(text))
+        print(f"Output:{figlet.renderText(text)}")
     else:
         list = figlet.getFonts()
         f = random.choice(list)
         figlet.setFont(font=f)
-        print(figlet.renderText(text))
+        print(f"Output:{figlet.renderText(text)}")
 else:
     sys.exit
     print("Invalid usage")
