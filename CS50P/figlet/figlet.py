b38aeff45d4b77efe4075e636ad96344b9f796ef
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2024-02-06 16:07:57 +0000

    Wed, Feb 7, 2024, 12:07 AM +08:00

--- CS50P/figlet/figlet.py
+++ CS50P/figlet/figlet.py
@@ -6,12 +6,13 @@ figlet = Figlet()
 
 
 if len(sys.argv) == 1 or len(sys.argv) == 3:
-    text = input("Input:")
     if len(sys.argv) == 3 and (sys.argv[1] == "-f" or sys.argv[1] == "--font"):
+        text = input("Input:")
         f = sys.argv[2]
         figlet.setFont(font=f)
         print(f"Output:{figlet.renderText(text)}")
     else:
+        text = input("Input:")
         list = figlet.getFonts()
         f = random.choice(list)
         figlet.setFont(font=f)
