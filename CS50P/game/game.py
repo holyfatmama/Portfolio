b86ffe158021845d019c099b57e8809676d22136
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2024-02-12 07:20:42 +0000

    Mon, Feb 12, 2024, 3:20 PM +08:00

--- CS50P/game/game.py
+++ CS50P/game/game.py
@@ -6,9 +6,11 @@ level = input("Level: ")
 
 
 if int(level) > 0:
-    answer = random.randint(0, level)
+    answer = random.randint(0, int(level))
     guess = input("Guess: ")
-    if guess == answer:
+    print(answer)
+    print(guess)
+    if int(guess) == answer:
         print("correct")
     else:
         print("wrong")
