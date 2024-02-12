db50243733eae469aaec8d33bafc705245d27f9a
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2024-02-12 09:06:51 +0000

    Mon, Feb 12, 2024, 5:06 PM +08:00

--- CS50P/professor/professor.py
+++ CS50P/professor/professor.py
@@ -6,7 +6,13 @@ def main():
 
 
 def get_level():
-    ...
+    level = int(input("Level: "))
+    while True:
+        try:
+            if 1 <= level <= 3:
+                return level
+        except ValueError:
+            print("input an integer between 1 and 3")
 
 
 def generate_integer(level):
