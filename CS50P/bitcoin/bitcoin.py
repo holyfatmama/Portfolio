7a55247c46a9d41e507b26b1c47a9a5dcadc06d7
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2024-02-12 12:12:48 +0000

    Mon, Feb 12, 2024, 8:12 PM +08:00

--- CS50P/bitcoin/bitcoin.py
+++ CS50P/bitcoin/bitcoin.py
@@ -2,11 +2,12 @@ import requests
 import json
 import sys
 
-#try:
-r = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-q = r.text
-w = json.loads(q)
-print(w["bpi"]["USD"]["rate"])
+try:
+    r = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+    q = r.text
+    w = json.loads(q)
+    amount = w["bpi"]["USD"]["rate"]
+    print("$" + amount)
 
-#except requests.RequestException:
-
+except requests.RequestException:
+    sys.exit()
