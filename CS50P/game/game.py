9580862bf482f518808d6970dc133c3de02f1964
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2024-02-12 07:16:45 +0000

    Mon, Feb 12, 2024, 3:16 PM +08:00

--- CS50P/game/game.py
+++ CS50P/game/game.py
@@ -5,11 +5,15 @@ import random
 while True:
     try:
         level = input("Level: ")
-        if level > 0:
+        if int(level) > 0:
             answer = random.randint(1, level)
             guess = input("Guess: ")
+            if guess == answer:
+                print("correct")
+            else:
+                print("wrong")
             break
     except:
         print("input an integer")
-
+        break
 
