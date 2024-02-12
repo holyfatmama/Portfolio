f772eebd1aea2b2aca40e75e66172d07c1bf5d3e
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2024-02-12 06:45:41 +0000

    Mon, Feb 12, 2024, 2:45 PM +08:00

--- CS50P/adieu/adieu.py
+++ CS50P/adieu/adieu.py
@@ -1,6 +1,12 @@
+import inflect
 
+p = inflect.engine()
+
+names = []
 while True:
     try:
-
+        name = input("Name: ")
+        names.appened(name)
     except EOFError:
+        print("Adieu, Adieu, to", )
 
