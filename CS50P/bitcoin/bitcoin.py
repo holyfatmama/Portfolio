9e7c7bc9bc7093cc51d832368d39f0ab45b00e71
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2024-02-14 12:38:43 +0000

    Wed, Feb 14, 2024, 8:38 PM +08:00

--- CS50P/bitcoin/bitcoin.py
+++ CS50P/bitcoin/bitcoin.py
@@ -4,12 +4,14 @@ import sys
 
 while True:
     try:
-        r = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-        w = r.json()
-
-        amount = w["bpi"]["USD"]["rate"]
-        print(w)
-        break
-
-    except requests.RequestException:
-        sys.exit()
+        amount = sys.argv[1]
+        try:
+            r = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+            w = r.json()
+            amount = w["bpi"]["USD"]["rate"]
+            print(w)
+            break
+        except requests.RequestException:
+            sys.exit()
+    except:
+        print("please input correct arguments")
