4e6d36d69c97e7f5e63be509e6d641e2d98ce8ff
holyfatmama <135823843+holyfatmama@users.noreply.github.com>
2024-02-14 13:06:46 +0000

    Wed, Feb 14, 2024, 9:06 PM +08:00

--- CS50P/bitcoin/bitcoin.py
+++ CS50P/bitcoin/bitcoin.py
@@ -10,10 +10,12 @@ while True:
             sys.exit(1)
         else:
             try:
-                amount = float(sys.argv[1])
+                number = float(sys.argv[1])
                 r = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
                 data = r.json()
-                print(json.dumps(r.json(), indent = 2))
+                price = data["bpi"]["USD"]["rate_float"]
+                amount = number * price
+                print(f"${amount:,.4f}")
                 break
             except ValueError:
                 print("Command-line argument is not a number")
